0796469b69361a2e78c4335ee1f9765c8d6adf10
Sukh Sagar Shukla <sss.sukhsagar@gmail.com>
2025-12-18 17:29:02 +0000

    Added the flask project

--- app.py
+++ app.py
@@ -1,42 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-JOBS = [
-    {
-        'id': 1,
-        'title': 'Software Engineer',
-        'location': 'San Francisco, CA',
-        'salary': '$120,000 - $180,000'
-    },
-    {
-        'id': 2,
-        'title': 'Product Manager',
-        'location': 'New York, NY',
-        'salary': '$100,000 - $150,000'
-    },
-    {
-        'id': 3,
-        'title': 'Data Analyst',
-        'location': 'Remote',
-        'salary': '$80,000 - $120,000'
-    },
-    {
-        'id': 4,
-        'title': 'UX Designer',
-        'location': 'Austin, TX',
-        'salary': '$90,000 - $130,000'
-    }
-]
 
-@app.route('/')
-def home():
-    return render_template('home.html', jobs=JOBS, company_name='Acme Corp')
+@app.route("/")
+def hello_world():
+  return "Hello, Sam!"
 
-@app.route('/job/<int:job_id>')
-def job_detail(job_id):
-    job = next((j for j in JOBS if j['id'] == job_id), None)
-    return render_template('job.html', job=job, company_name='Acme Corp')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
